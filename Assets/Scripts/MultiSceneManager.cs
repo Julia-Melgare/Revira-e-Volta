6049a8f917bc445334eae449ea90b807e586cd40
Diogo45 <diogohartmann45@gmail.com>
2021-10-17 20:19:51 -0300

    Go to next scene

--- Assets/Scripts/MultiSceneManager.cs
+++ Assets/Scripts/MultiSceneManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,7 +14,26 @@ public class MultiSceneManager : MonoBehaviour
     private void OnEnable()
     {
         RestartLevel.onRestartLevel += OnRestart;
+        LevelStateController.onGameWin += OnWin;
+
     }
+
+    private void OnWin(bool win)
+    {
+        StartCoroutine(WinDelay());
+    }
+
+    IEnumerator WinDelay()
+    {
+
+        yield return new WaitForSeconds(5f);
+        sceneID++;
+
+        PlayerPrefs.SetInt("Level", sceneID);
+
+        LoadLevel(sceneID);
+    }
+    
 
     /*private void OnDisable()
     {
@@ -22,14 +42,23 @@ public class MultiSceneManager : MonoBehaviour
 
     void Start()
     {
-        //sceneID = PlayerPrefs.GetInt("Level", 3);
-        LoadLevel(PlayerPrefs.GetInt("Level", 3));
+        Scene manager = SceneManager.GetSceneByName("SceneManager");
+
+        DontDestroyOnLoad(gameObject);
+
+        sceneID = PlayerPrefs.GetInt("Level", 3);
+        LoadLevel(sceneID);
         SceneManager.SetActiveScene(SceneManager.GetSceneAt(1));
     }
 
     void OnRestart(InputValue inputValue)
     {
         Reload();
+    }
+
+    void ReloadSelf()
+    {
+
     }
 
     public void Reload()
@@ -40,10 +69,15 @@ public class MultiSceneManager : MonoBehaviour
 
     public void LoadLevel(int ID)
     {
+
         SceneManager.LoadScene(ID);
 
         SceneManager.LoadScene("Player", LoadSceneMode.Additive);
 
         SceneManager.LoadScene("UI", LoadSceneMode.Additive);
+
+
+
+
     }
 }
